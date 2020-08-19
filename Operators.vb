3ff8e4e5147845091ce68eb16bd9776f0555a985
Lane Coleman <lane.coleman@live.com>
2020-08-19 08:46:59 -0600

    Add project files.

---
+++ Operators.vb
@@ -0,0 +1,23 @@
+﻿Module Operators
+    'Lane Coleman
+    'RCET0265
+    'Fall2020
+    'Operators
+    'repo
+    Sub Main()
+        'Let's do math!!
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(7 - 1)
+        Console.ReadLine()
+        Console.WriteLine(5 * 5)
+        Console.ReadLine()
+        Console.WriteLine(7 / 3)
+        Console.ReadLine()
+        Console.WriteLine(7 \ 3)
+        Console.ReadLine()
+        Console.WriteLine(70 Mod 30)
+        Console.ReadLine()
+    End Sub
+
+End Module
